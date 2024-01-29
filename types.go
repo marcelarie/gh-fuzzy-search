03ecb8643fee9e31416dee91b6f777a64ea90477
marcelarie <googlillo@gmail.com>
2024-01-29 18:41:56 +0100

    add types

---
+++ types.go
@@ -0,0 +1,13 @@
+package main
+
+type Color string
+
+const (
+	ColorRed    Color = "\033[31m"
+	ColorGreen        = "\033[32m"
+	ColorBlue         = "\033[34m"
+	ColorYellow       = "\033[33m"
+	ColorPurple       = "\033[35m"
+	ColorCyan         = "\033[36m"
+	ColorWhite        = "\033[37m"
+)
